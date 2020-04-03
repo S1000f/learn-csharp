a29cf65dfb0e31f25506d2a2799ea5e17e02e580
dohpkim <dohp.kim@gmail.com>
2020-04-03 16:52:12 +0900

    go to home

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,7 +23,15 @@
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
             this.button_test = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button_test
@@ -37,12 +45,79 @@
             this.button_test.UseVisualStyleBackColor = true;
             this.button_test.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(315, 152);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(141, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(315, 180);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(141, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 217);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(480, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "history";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(315, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "1~299 숫자 맞추기 게임";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 426);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Tick";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button_test);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -54,6 +129,13 @@
         #endregion
 
         private System.Windows.Forms.Button button_test;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,10 +14,11 @@ namespace WindowsFormsApp1 {
 
         Button button2;
         int num = 0;
-        
+        int quizNum;
+        int reset;
+
         public Form1() {
             InitializeComponent();
-            MessageBox.Show("Hello world!");
 
             Button button = new Button();
             button.Location = new Point(100, 100);
@@ -33,6 +34,9 @@ namespace WindowsFormsApp1 {
             button_test.Text = $"see {20 + 5} times";
             button_test.Text = String.Format("behold this {0}", 30 + 5);
 
+            Random random = new Random();
+            quizNum = random.Next(1, 300);
+
         }
 
         private void button1_Click(object sender, EventArgs e) {
@@ -47,9 +51,37 @@ namespace WindowsFormsApp1 {
                 button2.Width = 100;
                 Thread.Sleep(10);
                 button2.Visible = ran.Next(0, 3) == 0 ? true : false;
-
             }
             
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e) {
+
+            int answer;
+            //answer = int.Parse(textBox1.Text);
+            int.TryParse(textBox1.Text, out answer); // NumberFormatException 발생시 알아서 예외처리 해준다. 예외 발생시 0으로 out. 메소드 자체는 불린값 반환
+            if(quizNum == answer) {
+                label1.Text = "정답입니다!!!";
+            } else if(quizNum > answer) {
+                label1.Text = $"\n{answer} 보다 큰 숫자입니다\n";
+                label2.Text += label1.Text;
+            } else {
+                label1.Text = $"\n{answer} 보다 작은 숫자입니다" + Environment.NewLine; // 운영체제와 상관없이 항상 줄바꿈 가능
+                label2.Text += label1.Text;
+            }
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            label4.Text = "Tok";
+            reset = 1;
         }
     }
 }
